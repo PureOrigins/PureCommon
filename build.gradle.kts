845efcefd3e5658ba83096ca6c0d5ef5317e8392
Michi Palazzo <palazzomichi@gmail.com>
2022-02-19 19:07:46 +0100

    Update

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,8 @@ repositories {
     mavenCentral()
     maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
     mavenLocal()
+    maven("https://oss.sonatype.org/content/repositories/snapshots")
+    maven("https://oss.sonatype.org/content/repositories/central")
 }
 
 dependencies {
