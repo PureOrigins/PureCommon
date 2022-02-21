da619760104ee47ebe154a9efe9aff5b95d3533d
Michi Palazzo <palazzomichi@gmail.com>
2022-02-21 18:02:22 +0100

    Fix jitpack

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,7 @@ repositories {
     mavenCentral()
     maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
     maven("https://repo.codemc.io/repository/nms/")
+    maven("https://libraries.minecraft.net/")
 }
 
 dependencies {
