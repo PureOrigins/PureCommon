575123889568c4e15478454ef727f4d76f28c428
Raymark <raymark.ra@gmail.com>
2022-02-23 09:48:32 +0100

    added main class

---
+++ src/main/kotlin/it/pureorigins/common/PureCommon.kt
@@ -0,0 +1,13 @@
+package it.pureorigins.common
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class PureCommon : JavaPlugin() {
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
