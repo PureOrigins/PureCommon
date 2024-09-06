8f43ca98d03ad72aca39df61a92ca7f3288bb623
Raymark97 <raymark.ra@gmail.com>
2024-09-06 10:41:25 +0200

    temporarily removed nms

--- src/main/kotlin/it/pureorigins/common/PureCommon.kt
+++ src/main/kotlin/it/pureorigins/common/PureCommon.kt
@@ -1,28 +1,11 @@
 package it.pureorigins.common
 
-import com.mojang.brigadier.Command
-import com.mojang.brigadier.arguments.StringArgumentType.string
-import com.mojang.brigadier.suggestion.SuggestionProvider
-import io.papermc.paper.command.brigadier.Commands
-import io.papermc.paper.command.brigadier.argument.ArgumentTypes
-import org.bukkit.Bukkit
-import org.bukkit.entity.Player
 import org.bukkit.plugin.java.JavaPlugin
 
 class PureCommon : JavaPlugin() {
     override fun onEnable() {
         val config = json.readFileAs(file("config.json"), DatabaseCommand.Config())
-        overrideComponentSerializer()
+        //overrideComponentSerializer()
         DatabaseCommand(this, config).register()
-
-        //command example
-        val command = Commands.literal("new-command").requiresPermission("purecommon.new-command").executes {
-            it.source.sendNullableMessage("Hello, world!")
-            Command.SINGLE_SUCCESS
-        }.then(Commands.argument("name", ArgumentTypes.player()).suggests { context, builder ->
-            builder.suggest("Notch").suggest("pippo").buildFuture()
-        }).executes {
-            return@executes Command.SINGLE_SUCCESS
-        }
     }
 }
